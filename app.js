9fbe194d956f310da6d93934ad5e52a2b28ad87d
celineblle <celineblle@outlook.fr>
2023-11-27 11:38:19 +0100

    quete Express 01.1

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const allMovies = (req, res) => {
+  res.status(200).json(movies);
+};
+
+app.get("/api/movies", allMovies);
+
+const movie = (req, res) => {
+  const findMovie = movies.find((element) => element.id == req.params.id);
+  if (findMovie !== undefined) {
+    res.status(200).json(findMovie);
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+
+app.get("/api/movies/:id", movie);
+
+module.exports = app;
---
+++ tests/movies.test.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET/api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET/api/movies/1", () => {
+  it("should return one movie", async () => {
+    const response = await request(app).get("/api/movies/1");
+    expect(response.status).toEqual(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+});
+
+describe("GET/api/movies/5", () => {
+  it("should return not found", async () => {
+    const response = await request(app).get("/api/movies/5");
+    expect(response.status).toEqual(404);
+  });
+});
